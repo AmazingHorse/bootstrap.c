bb9d09eb9e052a5210fccf227484dd0ba77dc2b6
Bill Heughan <billheughan@gmail.com>
2015-07-22 16:00:10 -0400

    Added googletest testing with 'make check'

---
+++ {{cookiecutter.project_name|lower}}/src/sample.c
@@ -0,0 +1,5 @@
+#include "autotools-unittest/sample.h"
+
+int square(int n) {
+  return n * n;
+}
---
+++ {{cookiecutter.project_name|lower}}/src/sample.h
@@ -0,0 +1,27 @@
+#ifndef SRC_AUTOTOOLS_UNITTEST_SAMPLE_H_
+#define SRC_AUTOTOOLS_UNITTEST_SAMPLE_H_
+
+/**
+ * @brief Calculate the square of given number.
+ *
+ * @param [in] n A number.
+ *
+ * @return The square of given number n.
+ */
+int square(int n);
+
+/**
+ * @brief Calculate the factorial of given number.
+ *
+ * @param [in] n A number.
+ *
+ * @return The factorial of given number n.
+ */
+inline int factorial(int n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n * factorial(n-1);
+}
+
+#endif  // SRC_AUTOTOOLS_UNITTEST_SAMPLE_H_
